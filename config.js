34ec34e39d5105c3afdb32ab2bce9e63cb14482d
dn1ghtm <discord.respawn@gmail.com>
2024-12-12 18:14:20 +0100

    updated

--- config.js
+++ config.js
@@ -1,1 +1,51 @@
- 
+const config = {
+    version: '1.0.0', // Current version of the application
+    githubConfigUrl: 'https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/config.js',
+};
+
+async function checkForUpdates() {
+    try {
+        const response = await fetch(config.githubConfigUrl);
+        const remoteConfigText = await response.text();
+        
+        // Extract version from remote config using regex
+        const versionMatch = remoteConfigText.match(/version:\s*['"](.+?)['"]/);
+        if (!versionMatch) {
+            return {
+                success: false,
+                error: 'Could not find version in remote config'
+            };
+        }
+
+        const remoteVersion = versionMatch[1];
+        const localVersion = config.version;
+
+        return {
+            success: true,
+            currentVersion: localVersion,
+            remoteVersion: remoteVersion,
+            updateAvailable: isNewerVersion(remoteVersion, localVersion)
+        };
+    } catch (error) {
+        return {
+            success: false,
+            error: error.message
+        };
+    }
+}
+
+function isNewerVersion(remote, local) {
+    const remoteParts = remote.split('.').map(Number);
+    const localParts = local.split('.').map(Number);
+
+    for (let i = 0; i < 3; i++) {
+        if (remoteParts[i] > localParts[i]) return true;
+        if (remoteParts[i] < localParts[i]) return false;
+    }
+    return false;
+}
+
+module.exports = {
+    config,
+    checkForUpdates
+}; 
