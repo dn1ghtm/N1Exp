20fa0f63221d519a7929812970fcad33eace9b5b
N1ghtm <106837073+dn1ghtm@users.noreply.github.com>
2024-12-12 18:24:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    version: '1.0.1', // Current version of the application
+    version: '1.0.2', // Current version of the application
     githubConfigUrl: 'https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/config.js',
 };
 
